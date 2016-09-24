ce90d467172ebd37ef638b39813fbf64f3e0872b
gqdw <aca_jingru@qq.com>
2016-09-24 20:00:33 +0800

    zabbix tool of del host by ip

---
+++ del_host_by_ip.py
@@ -0,0 +1,16 @@
+from zabbix import Zabbix
+import sys
+
+def main():
+	if len(sys.argv) != 2:
+		print 'useage: %s [thehostip]' % (sys.argv[0])
+		sys.exit(1)
+	Z = Zabbix()
+	Z.get_auth()
+	Z.get_hostip()
+	ip = sys.argv[1]
+	Z.del_host_from_ip(ip)
+
+
+if __name__ == '__main__':
+	main()
